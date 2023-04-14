765f7776b238d74c507a5f197b6d0dc0b17e7dcf
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-14 19:04:37 +0530

    modify files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
+  const [percentage, setPercentage] = useState(35);
+
   return (
     <div className="App">
-      
+      <input
+        type="range"
+        min="1"
+        max="100"
+        step="1"
+        value={percentage}
+        onChange={(e) => setPercentage(e.target.value)}
+      />
     </div>
   );
 }
