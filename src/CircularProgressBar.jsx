09be027f4cdc2290886b00d96346360e621b4861
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-14 19:17:01 +0530

    modify files

--- src/CircularProgressBar.jsx
+++ src/CircularProgressBar.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React from "react";
 
 export default function CircularProgressBar({ percentage, circleWidth }) {
+  const radius = 85;
+
   return (
     <div>
-      <svg width={circleWidth} height={circleWidth} ></svg>
+      <svg
+        width={circleWidth}
+        height={circleWidth}
+        viewBox={`0 0 ${circleWidth} ${circleWidth}`}
+      >
+        <circle
+          cx={circleWidth / 2}
+          cy={circleWidth / 2}
+          strokeWidth="15px"
+          r={radius}
+        />
+      </svg>
     </div>
-  )
+  );
 }
